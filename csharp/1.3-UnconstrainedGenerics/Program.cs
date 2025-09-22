1b19becda8b5b0e91987b853cf0e379d2cbcb9ff
Christian Nagel <christian@christiannagel.com>
2025-09-22 20:32:01 +0200

    nameof with unconstrainted generic

---
+++ csharp/1.3-UnconstrainedGenerics/Program.cs
@@ -0,0 +1,5 @@
+﻿string name = nameof(List<>);
+string oldName = nameof(List<string>);
+
+Console.WriteLine($"{name} - using unconstrained generic"); // List
+Console.WriteLine($"{oldName} - not unconstrained: previous version with the same result, but why add a type?");
