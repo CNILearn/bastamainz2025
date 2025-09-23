cea8ecc7f9d95f5bb68ea18dbdf754d665fdfeb4
Christian Nagel <christian@christiannagel.com>
2025-09-23 07:37:27 +0200

    folder reorg, term update

--- csharp/1.1-Beep/Program.cs
+++ csharp/1.1-Beep/Program.cs
@@ -1,6 +1,8 @@
 ﻿for (int i = 0; i < 3; i++)
 {
     Console.Beep();
+    Console.Write("Beep.. ");
     await Task.Delay(1400);
 }
+Console.WriteLine();
 Console.WriteLine("Welcome to BASTA! Mainz 2025!");
--- csharp/1.3-UnconstrainedGenerics/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿string name = nameof(List<>);
-string oldName = nameof(List<string>);
-
-Console.WriteLine($"{name} - using unconstrained generic"); // List
-Console.WriteLine($"{oldName} - not unconstrained: previous version with the same result, but why add a type?");
